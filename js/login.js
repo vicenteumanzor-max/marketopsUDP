abb46cf0a9c714ae061d004d8509d4b474ae0881
Vice <vicente.umanzor@mail.udp.cm>
2026-09-02 23:18:26 -0400

    Se modificó el index para que fuese la página de inicio de sesión, se creó un archivo para el registro de cuenta y otro para la información de la compañia

---
+++ js/login.js
@@ -0,0 +1,9 @@
+const form = document.getElementById("login-form");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+const passwordInput = document.getElementById("password").value;
+const mensajeError = document.getElementById("mensaje-error");
+if(){
+    
+}
+}); 
--- js/registro.js
+++ js/registro.js
@@ -1,3 +1,4 @@
+
 const form = document.getElementById("registration-form");
 form.addEventListener("submit", function(event){
     event.preventDefault();
