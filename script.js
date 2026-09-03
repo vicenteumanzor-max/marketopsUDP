0e597aa567d089fa2525de4b959cb9d9c4510b99
Vice <vicente.umanzor@mail.udp.cm>
2026-09-02 22:33:20 -0400

    Actualización de script y modificación mensaje de error

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
+const form = document.getElementById("registration-form");
+form.addEventListener("submit", function(event){
+    event.preventDefault();
 const passwordInput = document.getElementById("password").value;
 const confirmPasswordInput = document.getElementById("confirm-password").value;
+const mensajeError = document.getElementById("mensaje-error");
 if(passwordInput.length < 8 || /\d/.test(passwordInput) === false ||    /[A-Z]/.test(passwordInput) === false || /[a-z]/.test(passwordInput) === false){
-    alert("La contraseña debe tener estos campos: al menos 8 caracteres, un número, una letra mayúscula y una letra minúscula.");
+    mensajeError.textContent = "La contraseña debe tener estos campos: al menos 8 caracteres, un número, una letra mayúscula y una letra minúscula.";
     return false;
 }
 if(passwordInput !== confirmPasswordInput){
-    alert("Las contraseñas no coinciden.");
+    mensajeError.textContent = "Las contraseñas no coinciden.";
     return false;
 }
+mensajeError.textContent = "";
+});
