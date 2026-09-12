1c6bb5f505a8a36d5cad528f4435a0bca40678f8
Vice <vicente.umanzor@mail.udp.cm>
2026-09-11 23:57:21 -0300

    Eliminado

--- js/login.js
+++
@@ -1,9 +0,0 @@
-const form = document.getElementById("login-form");
-form.addEventListener("submit", function(event){
-    event.preventDefault();
-const passwordInput = document.getElementById("password").value;
-const mensajeError = document.getElementById("mensaje-error");
-if(){
-    
-}
-}); 
